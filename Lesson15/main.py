091da90cc1d236ca69f51a68d863634e4006e14c
aspahija <ante.spahija@gmail.com>
2018-07-01 17:39:07 +0200

    Lesson15

---
+++ Lesson15/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        return self.response.write('Hello SmartNinja!')
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/admin', MainHandler),
+], debug=True)
